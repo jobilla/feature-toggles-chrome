b088a94d7416b9d988df811dd4581eb8a868b1da
Ulrik Hovstad <ulrik.hovstad@jobilla.com>
2023-06-29 14:08:16 +0200

    Upgrade manifest n everything

--- src/background.js
+++ src/background.js
@@ -1,36 +1,37 @@
 const tabs = new Map();
 
 function renderFeatures(tabId) {
-  if (tabs.has(tabId)) {
-    chrome.storage.local.set({ features: tabs.get(tabId) });
-  } else {
-    chrome.storage.local.set({ features: null });
-  }
+    if (tabs.has(tabId)) {
+        chrome.storage.local.set({ features: tabs.get(tabId) });
+    } else {
+        chrome.storage.local.set({ features: null });
+    }
 }
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  switch (request.event) {
-    case "usesFeatureToggle":
-      if (!tabs.has(sender.tab.id)) {
-        tabs.set(sender.tab.id, {});
-      }
-      if (sender.tab.active) {
-        renderFeatures(sender.tab.id);
-      }
-      sendResponse(true);
-      break;
-    case "featureDiscovery":
-      tabs.set(sender.tab.id, request.features);
-      renderFeatures(sender.tab.id);
-      break;
-  }
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    switch (request.event) {
+        case "usesFeatureToggle":
+            if (!tabs.has(sender.tab.id)) {
+                tabs.set(sender.tab.id, {});
+            }
+            if (sender.tab.active) {
+                renderFeatures(sender.tab.id);
+            }
+            sendResponse(true);
+            break;
+        case "featureDiscovery":
+            tabs.set(sender.tab.id, request.features);
+            renderFeatures(sender.tab.id);
+            break;
+    }
+    return true;
 });
 
-chrome.tabs.onActivated.addListener(async (tab) => {
-  const { tabId } = tab;
-  if (tab && tab.hasOwnProperty("tabId")) {
+chrome.action.onClicked.addListener((tab) => {
+    renderFeatures(tab.id);
+});
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+    const {tabId} = activeInfo;
     renderFeatures(tabId);
-  } else {
-    renderFeatures(tab);
-  }
 });
--- src/plugin.js
+++ src/plugin.js
@@ -1,31 +1,35 @@
 if (
-  localStorage.getItem("_feature._enabled") !== null &&
-  JSON.parse(localStorage.getItem("_feature._enabled")) === true
+    localStorage.getItem("_feature._enabled") !== null &&
+    JSON.parse(localStorage.getItem("_feature._enabled")) === true
 ) {
-  chrome.runtime.sendMessage({ event: "usesFeatureToggle" });
+    chrome.runtime.sendMessage({event: "usesFeatureToggle"}).then((response) => {
+        console.log("Feature toggle message sent", response);
+    });
 }
 
 const features = {};
 const broadcastFeatures = () => {
-  Object.keys(localStorage)
-    .filter((key) => key.startsWith("_feature.") && key !== "_feature._enabled")
-    .forEach((feature) => {
-      let feat = localStorage.getItem(feature);
-      if (typeof feat === "string") {
-        features[feature] = JSON.parse(feat);
-      } else {
-        features[feature] = feat;
-      }
-    });
-  chrome.runtime.sendMessage({ event: "featureDiscovery", features });
+    Object.keys(localStorage)
+        .filter((key) => key.startsWith("_feature.") && key !== "_feature._enabled")
+        .forEach((feature) => {
+            console.log('found feature', feature);
+            let feat = localStorage.getItem(feature);
+            if (typeof feat === "string") {
+                features[feature] = JSON.parse(feat);
+            } else {
+                features[feature] = feat;
+            }
+        });
+    chrome.runtime.sendMessage({event: "featureDiscovery", features});
 };
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.event === "featureToggled") {
-    localStorage.setItem(message.feature, JSON.stringify(message.value));
-    sendResponse(true);
-    broadcastFeatures();
-  }
+    if (message.event === "featureToggled") {
+        localStorage.setItem(message.feature, JSON.stringify(message.value));
+        sendResponse(true);
+        broadcastFeatures();
+    }
+    return true;
 });
 
 broadcastFeatures();
